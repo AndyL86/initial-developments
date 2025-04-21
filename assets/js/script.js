443b0549533702a145342885baeb9f0f073c00cb
AndyL86 <alinehan89@outlook.com>
2025-04-21 16:18:37 +0100

    add gameboy js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,34 @@ document.querySelectorAll("#about > div.about-details-section > p").forEach((abo
         aboutText.innerHTML = "In this time, I have built up my skillset and contributed to a real world, large scale retail business. I am keen to expand my skills, knowledge and experience!";
       })
 });
+
+document.querySelectorAll("#controller_up").forEach((upButton) => {
+  upButton.addEventListener('click', () => {
+    console.log("Up button pressed!")
+  })
+});
+document.querySelectorAll("#controller_right").forEach((rightButton) => {
+  rightButton.addEventListener('click', () => {
+    console.log("Right button pressed!")
+  })
+});
+document.querySelectorAll("#controller_down").forEach((downButton) => {
+  downButton.addEventListener('click', () => {
+    console.log("Down button pressed!")
+  })
+});
+document.querySelectorAll("#controller_left").forEach((leftButton) => {
+  leftButton.addEventListener('click', () => {
+    console.log("Left button pressed!")
+  })
+});
+document.querySelectorAll("#controller_a").forEach((aButton) => {
+  aButton.addEventListener('click', () => {
+    console.log("A button pressed!")
+  })
+});
+document.querySelectorAll("#controller_b").forEach((bButton) => {
+  bButton.addEventListener('click', () => {
+    console.log("B button pressed!")
+  })
+});
