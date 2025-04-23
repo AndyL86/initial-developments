61df260e186b2074cfa3ae74bf196ac80099d40b
AndyL86 <alinehan89@outlook.com>
2025-04-23 12:17:45 +0100

    add profile menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,49 @@
+
+const profileDetails = [{
+  category: "Work Experience",
+  experience: [
+    {company: "Mamas and Papas", role: "Shopify Front End Developer", date: "October 2023 - March 2025"},
+    {company: "Project Kahn", role: "Product Manager", date: "March 2021 - July 2023"},
+    {company: "LTE Scientific", role: "Purchasing Manager", date: "March 2020 - March 2021"}
+  ]
+},
+{
+  category: "Qualifications",
+  qualifications: [
+    {institution: "The Code Institute", qualification: "Full Stack Web Development", grade: "Diploma - Merit", date: "2022 - 2023"},
+    {institution: "Myerscough College", qualification: "Motorsport Supply Chain Management and Logistics", grade: "First Class BSc (Hons)", date: "2013 - 2016"}
+  ]
+},
+{
+  category: "Coding Languages",
+  languages: [
+    {language: ["HTML", "CSS", "Javascript", "Python"]},
+    {frameworks: ["Shopify Liquid", "Bootstrap", "Tailwind", "JQuery", "ReactJS", "Django"]},
+    {version: ["GIT", "SourceTree", "Bitbucket"]},
+    {analytics: ["Google analytics", "Google tag manager", "Dynamic Yield"]}
+  ]
+}
+]
+
+
 document.querySelectorAll("#about > div.about-details-section > p").forEach((aboutText) => {
       aboutText.addEventListener('click', () => {
         aboutText.innerHTML = "In this time, I have built up my skillset and contributed to a real world, large scale retail business. I am keen to expand my skills, knowledge and experience!";
       })
 });
+
+document.querySelectorAll(".screen").forEach((text) => {
+  const menuOption = profileDetails.filter((value) => value.category);
+  for (i = 0; i < menuOption.length; i++) {
+    const option = menuOption[i].category;
+    text.innerHTML += '<p>' + option + '</p>';
+    }
+})
+
+const highlight = document.querySelectorAll(".screen > p");
+for (i = 0; i < highlight.length; i++) {
+  highlight[0].classList.toggle("active");
+}
 
 document.querySelectorAll("#controller_up").forEach((upButton) => {
   upButton.addEventListener('click', () => {
